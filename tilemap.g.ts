157b96eef6617b6a208740f4ee3543e760ef0e7c
ambrthedev <74512496+ambrthedev@users.noreply.github.com>
2020-11-15 18:37:31 -0600

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`1000100002030303030303030303030303030304010909090909090909090909090909050109090909090909090909090909090501090909090909090909090909090905010909090909090909090909090909050109090909090909090909090909090501090909090909090909090909090905010909090909090909090909090909050109090909090909090909090909090501090909090909090909090909090905010909090909090909090909090909050109090909090909090909090909090501090909090909090909090909090905010909090909090909090909090909050109090909090909090909090909090506070707070707070707070707070708`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath4,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath9,sprites.castle.tilePath5], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
